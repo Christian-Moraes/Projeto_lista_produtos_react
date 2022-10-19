193f096343fb1655b19a24f1f3262e18a0f74fbd
Christian Matheus de Souza Moraes <83892899+Christian-Moraes@users.noreply.github.com>
2022-10-19 01:07:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Home} from './pages/Home';
 import {Cadastrar} from './pages/Cadastrar';
 import {Visualizar} from './pages/Visualizar';
-import {Login} from './pages/Login';
 
 function App() {
 
